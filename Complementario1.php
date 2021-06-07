b31d8f6eabb2a84ab3439254cc12db6038283ad1
¨MarlenySosa¨ <marrlen.sosa@gmail.com>
2021-06-06 22:36:10 -0600

    ¨ComplentariaG1¨

---
+++ Complementario1.php
@@ -0,0 +1,34 @@
+<html lang ="es">
+ <head>
+    <meta charset = "UFT-8">
+    <link rel="stylesheet" href="styleComp1.css">
+
+    <title>Estilos Embebidos</title>
+
+ </head>
+
+ <body >
+    <div>
+        <h2 >
+            Estilos embebidos en las etiquetas html
+        </h2>
+        <p > 
+        <strong>
+            Usar estilos css embebidos en las etiquetas HTML el peor y el menos utilizado
+            de los metodos para agregar reglas de estilo.
+            No es nada recomendable por la dificultad que implica realizar un cambio posterior.
+        </strong>
+        </p>
+        <p > 
+            
+            Esta forma de incluir css directamente en los elmentos html solo se utiliza en determinadas 
+            situaciones en las que se debe incluir un estilo muy especifico para un solo elemento concreto. 
+        </p>
+        <p> Para insertar un estilo css en un elemento se utiliza la propiedad
+            <span style = "font-style: italic;"> style </span> de html, que en ningun momento se debe 
+            confundir con la etiqueta <span style = "font-style: italic;"> &lt; style &gt;</span>
+            que se introduce en la cabecera del documento
+        </p>
+    </div>   
+ </body>
+ </html>
---
+++ Complementario3.php
@@ -0,0 +1,27 @@
+<html lang ="es">
+ <head>
+    <meta charset = "UFT-8">
+    <link rel="stylesheet" href="styleComp3.css">
+
+    <title>Inscripciones Ciclo III 2021</title>
+ </head>
+
+ <body>
+    <h1>Datos del estudiante</h1>
+    <div class = "Estudiante">
+        <pre class = "Datos"> 
+            Nombre: Marleny Sosa
+            Carnet:MS132476
+            Numero de materias inscritas: 1
+        </pre>
+        <pre class = "Materias"> 
+            Materia: PHP
+            Matricula: 1
+            Grupo teorico: 01
+            Grupo practico: 01
+            Profesor: Alexander Siguenza
+        </pre>
+    </div>   
+ </body>
+ </html>
+ 
